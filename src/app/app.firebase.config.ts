2383945e538c9eef77f48abadd737875e5ee76bb
Sam <samong0105@gmail.com>
2019-07-15 15:00:36 +1200

    Adding firebase authentication and a login page

---
+++ src/app/app.firebase.config.ts
@@ -0,0 +1,9 @@
+export const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyDp7jL0bRTAG-6MkTBgECaHrD86Mv9vv8Y",
+    authDomain: "vault325.firebaseapp.com",
+    databaseURL: "https://vault325.firebaseio.com",
+    projectId: "vault325",
+    storageBucket: "",
+    messagingSenderId: "510196383810",
+    appId: "1:510196383810:web:cfa82e0fd3cd01c6"
+  };
