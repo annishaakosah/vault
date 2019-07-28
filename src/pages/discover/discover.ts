ac88d64347437f0a87a10511481c9d79c4aee475
Sam <samong0105@gmail.com>
2019-07-28 21:48:51 +1200

    Add infinite scroll for discover page

--- src/pages/discover/discover.ts
+++ src/pages/discover/discover.ts
@@ -12,6 +12,7 @@ import { DetailsPage } from '../details/details';
 })
 export class DiscoverPage {
   results;
+  page = 1;
   sort_by = "popularity.desc";
 
   constructor(
@@ -25,8 +26,22 @@ export class DiscoverPage {
     this.discover.discover();
   }
 
-  change_selection(sort_by: string) {
-    this.discover.discover(sort_by);
+  change_selection() {
+    this.page = 1
+    this.discover.discover(this.sort_by);
+  }
+
+  doInfinite(infiniteScroll) {
+    this.page = this.page+1;
+    
+    setTimeout(() => {
+      this.results = this.discover.discoverByPage(this.sort_by, this.page).then((data) => {
+        this.results = data
+      }, (err) => {
+        console.log("something went wrong");
+      });
+      infiniteScroll.complete();
+    }, 1000);
   }
 
   public getResults() {
