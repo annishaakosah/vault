9e97145ade20bb35d797171e2a5f691731a6a643
Sam <samong0105@gmail.com>
2019-07-29 15:24:50 +1200

    Add helper methods for details page

--- src/pages/details/details.ts
+++ src/pages/details/details.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { SearchTitleService } from '../../providers/search-title.service';
+import { ImdbService } from '../../providers/imdb-app.service';
 
 @IonicPage()
 @Component({
@@ -8,18 +9,22 @@ import { SearchTitleService } from '../../providers/search-title.service';
   templateUrl: 'details.html',
 })
 export class DetailsPage {
+  id:number
+  list
   title;
   loaded = false;
 
   constructor(
     private search: SearchTitleService,
+    private imdbService: ImdbService,
     public navCtrl: NavController,
     public navParams: NavParams) {
   }
 
   ionViewWillEnter() {
-    const id = this.navParams.get('id');
-    this.search.getByID(id).then((data) => {
+    this.id = this.navParams.get('id');
+    this.list = this.navParams.get('list')
+    this.search.getByID(this.id).then((data) => {
       this.title = data;
       this.loaded = true;
     }, (err) => {
@@ -28,7 +33,30 @@ export class DetailsPage {
     });
   }
 
-  ionViewWillLeave() {
-    // this.navCtrl.pop()
+  inList(){
+    switch(this.list){
+      case ("watchList"):
+        return this.imdbService.isInWatchList(this.id)
+      case "alreadyWatched":
+        return this.imdbService.isInAlreadyWatched(this.id)
+      default:
+        return false
+    }
+  }
+
+  removeFromWatchList(){
+    if(this.list == "alreadyWatched") {
+      console.error("You cannot perform this action")
+      return
+    }
+    this.imdbService.removeFromWatchList(this.id)
+  }
+
+  removeFromAlreadyWatched(){
+    if(this.list == "watchList") {
+      console.error("You cannot perform this action")
+      return
+    }
+    this.imdbService.removeFromAlreadyWatched(this.id)
   }
 }
