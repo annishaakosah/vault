3d36f3463572d1708172e53c16add55738137333
Annisha Akosah <akosahanni@ecs.vuw.ac.nz>
2019-08-10 17:34:44 +1200

    Progress bar now works based on actual episode/season counts

--- src/models/show.ts
+++ src/models/show.ts
@@ -4,9 +4,11 @@ export interface Show {
     seasons: any;
     airDate: string;
     voteAverage: number;
+    season_number: number;
     voteCount: number;
     overview: string;
     episodeRuntime: number;
     genres: string[];
     poster_path: string;
+    number_of_seasons: number;
 }
